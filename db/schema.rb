aee80bd25606ae22e1c1b0d5a0cf32d64ae04f65
Blari <Blarior@gmail.com>
2021-02-19 15:23:58 +0300

    Clear, created seeds file for simple data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_221728) do
+ActiveRecord::Schema.define(version: 2021_02_19_115453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,13 +33,6 @@ ActiveRecord::Schema.define(version: 2021_02_18_221728) do
   end
 
   create_table "skills", force: :cascade do |t|
-    t.string "title"
-    t.integer "percent_utilized"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "slills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
     t.datetime "created_at", precision: 6, null: false
