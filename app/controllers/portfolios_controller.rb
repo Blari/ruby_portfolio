d8c2dfc857d7e3ef7f05ed526a8b927b69db79a9
Blari <Blarior@gmail.com>
2021-02-21 20:42:37 +0300

    Add show functionality to portfolios page

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -23,7 +23,6 @@ class PortfoliosController < ApplicationController
 
   def edit
     @portfolio_item = Portfolio.find(params[:id])
-
   end
 
   def update
@@ -36,5 +35,9 @@ class PortfoliosController < ApplicationController
         format.html { render :edit, status: :unprocessable_entity }
       end
     end
+  end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
   end
 end
