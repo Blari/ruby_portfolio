109d19fbc1a7932df44e6291116a6f0459e24ba8
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2021-03-02 16:42:29 +0300

    Add default value for portfolio images

--- db/migrate/20210302085336_add_topic_reference_to_blogs.rb
+++ db/migrate/20210302085336_add_topic_reference_to_blogs.rb
@@ -1,5 +1,5 @@
 class AddTopicReferenceToBlogs < ActiveRecord::Migration[6.0]
   def change
-    add_reference :blogs, :topic, foreign_key: true
+    add_reference :blogs, :topic, :integer, foreign_key: true
   end
 end
